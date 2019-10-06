ce530f790502b9577e3c9880644f9ced05b77b45
Evgeni Raikhel <evgeni.raikhel@intel.com>
2019-10-06 16:19:40 +0300

    GLSLS dec wip

--- src/gl/decimation-gl.cpp
+++ src/gl/decimation-gl.cpp
@@ -1,26 +1,26 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+#include <iostream>
+#include <chrono>
+#include <strstream>
+//#include <glad/glad.h>
 
+//#include "opengl3.h"
 #include "option.h"
 #include "../include/librealsense2-gl/rs_processing_gl.hpp"
 #include "decimation-gl.h"
 
-#include <glad/glad.h>
-
-#include <iostream>
-#include <chrono>
-#include <strstream>
-
 using namespace rs2;
 using namespace librealsense::gl;
 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec2 textCoords;\n"
+static const char* decimation_fragment_shader_text =
+"#version 130\n"
+"in vec2 textCoords;\n"
 "uniform sampler2D textureSampler;\n"
-"uniform float opacity;\n"
-"uniform float width;\n"
-"uniform float height;\n"
+"uniform uint decimation_factor;\n"
+"uniform uint width;\n"
+"uniform uint height;\n"
+"precision mediump float;\n"
 "void main(void) {\n"
 "    float pixel_width = 1.0 / width;\n"
 "    float pixel_height = 1.0 / height;\n"
@@ -28,33 +28,35 @@ static const char* fragment_shader_text =
 "    float u = 0.0;\n"
 "    float v = 0.0;\n"
 "    float tex_y = 1.0 - textCoords.y;\n"
-"    if (mod(floor(gl_FragCoord.x), 2.0) == 0.0){\n"
-"        vec2 tx1 = vec2(textCoords.x, tex_y);\n"
-"        vec4 px1 = texture2D(textureSampler, tx1);\n"
-"        vec2 tx2 = vec2(textCoords.x + pixel_width, tex_y);\n"
-"        vec4 px2 = texture2D(textureSampler, tx2);\n"
-"        y = px1.x; u = px1.y; v = px2.y;\n"
-"    }\n"
-"    else\n"
-"    {\n"
-"        vec2 tx1 = vec2(textCoords.x - pixel_width, tex_y);\n"
-"        vec4 px1 = texture2D(textureSampler, tx1);\n"
-"        vec2 tx2 = vec2(textCoords.x, tex_y);\n"
-"        vec4 px2 = texture2D(textureSampler, tx2);\n"
-"        y = px2.x; u = px1.y; v = px2.y;\n"
-"    }\n"
-"    //y *= 256.0; u *= 256.0; v *= 256.0;\n"
-"    float c = y - (16.0 / 256.0);\n"
-"    float d = u - 0.5;\n"
-"    float e = v - 0.5;\n"
-"    vec3 color = vec3(0.0);\n"
-"    //color.x = clamp(((298.0 / 256.0) * c + (409.0 / 256.0) * e + 0.5), 0.0, 1.0);\n"
-"    //color.y = clamp(((298.0 / 256.0) * c - (100.0 / 256.0) * d - (208.0/256.0) * e + 0.5), 0.0, 1.0);\n"
-"    //color.z = clamp(((298.0 / 256.0) * c + (516.0 / 256.0) * d + 0.5), 0.0, 1.0);\n"
-"    color.x = clamp((y + 1.40200 * (v - 0.5)), 0.0, 1.0);\n"
-"    color.y = clamp((y - 0.34414 * (u - 0.5) - 0.71414 * (v - 0.5)), 0.0, 1.0);\n"
-"    color.z = clamp((y + 1.77200 * (u - 0.5)), 0.0, 1.0);\n"
-"    gl_FragColor = vec4(color.xyz, opacity);\n"
+"    //if (mod(floor(gl_FragCoord.x), 2.0) == 0.0){\n"
+"    //    vec2 tx1 = vec2(textCoords.x, tex_y);\n"
+"    //    vec4 px1 = texture2D(textureSampler, tx1);\n"
+"    //    vec2 tx2 = vec2(textCoords.x + pixel_width, tex_y);\n"
+"    //    vec4 px2 = texture2D(textureSampler, tx2);\n"
+"    //    y = px1.x; u = px1.y; v = px2.y;\n"
+"    //}\n"
+"    //else\n"
+"    //{\n"
+"    //    vec2 tx1 = vec2(textCoords.x - pixel_width, tex_y);\n"
+"    //    vec4 px1 = texture2D(textureSampler, tx1);\n"
+"    //    vec2 tx2 = vec2(textCoords.x, tex_y);\n"
+"    //    vec4 px2 = texture2D(textureSampler, tx2);\n"
+"    //    y = px2.x; u = px1.y; v = px2.y;\n"
+"    //}\n"
+"    ////y *= 256.0; u *= 256.0; v *= 256.0;\n"
+"    //float c = y - (16.0 / 256.0);\n"
+"    //float d = u - 0.5;\n"
+"    //float e = v - 0.5;\n"
+"    //vec3 color = vec3(0.0);\n"
+"    ////color.x = clamp(((298.0 / 256.0) * c + (409.0 / 256.0) * e + 0.5), 0.0, 1.0);\n"
+"    ////color.y = clamp(((298.0 / 256.0) * c - (100.0 / 256.0) * d - (208.0/256.0) * e + 0.5), 0.0, 1.0);\n"
+"    ////color.z = clamp(((298.0 / 256.0) * c + (516.0 / 256.0) * d + 0.5), 0.0, 1.0);\n"
+"    //color.x = clamp((y + 1.40200 * (v - 0.5)), 0.0, 1.0);\n"
+"    //color.y = clamp((y - 0.34414 * (u - 0.5) - 0.71414 * (v - 0.5)), 0.0, 1.0);\n"
+"    //color.z = clamp((y + 1.77200 * (u - 0.5)), 0.0, 1.0);\n"
+"    //gl_FragColor = vec4(color.xyz, opacity);\n"
+"    //Evgeni - todo\n"
+"    gl_FragColor = vec4(1., 1., 0., 0.5);\n"
 "}";
 
 class decimation_shader : public texture_2d_shader
@@ -63,7 +65,7 @@ public:
     decimation_shader()
         : texture_2d_shader(shader_program::load(
             texture_2d_shader::default_vertex_shader(), 
-            fragment_shader_text, "position", "textureCoords"))
+            decimation_fragment_shader_text, "position", "textureCoords"))
     {
         _width_location = _shader->get_uniform_location("width");
         _height_location = _shader->get_uniform_location("height");
@@ -90,7 +92,7 @@ void decimation_filter_gl::cleanup_gpu_resources()
 void decimation_filter_gl::create_gpu_resources()
 {
     _viz = std::make_shared<visualizer_2d>(std::make_shared<decimation_shader>());
-    _fbo = std::make_shared<fbo>(_width, _height);
+    _fbo = std::make_shared<fbo>(_padded_width, _padded_height);
     _enabled = glsl_enabled() ? 1 : 0;
 }
 
@@ -121,7 +123,7 @@ rs2::frame decimation_filter_gl::process_frame(const rs2::frame_source& src, con
     {
         perform_gl_action([&]()
         {
-            _fbo = std::make_shared<fbo>(_width, _height);
+            _fbo = std::make_shared<fbo>(_padded_height, _padded_height);
         }, [this] {
             _enabled = false;
         });
@@ -151,7 +153,7 @@ rs2::frame decimation_filter_gl::process_frame(const rs2::frame_source& src, con
             }
             case RS2_STREAM_INFRARED:
             case RS2_STREAM_FISHEYE:
-                tgt_type            = RS2_EXTENSION_VIDEO_FRAME_GL;
+                tgt_type = RS2_EXTENSION_VIDEO_FRAME_GL;
                 gl_internal_format  = GL_RED;
                 gl_internal_type = val_in_range(format, { RS2_FORMAT_RGB8, RS2_FORMAT_BGR8 }) ? GL_BYTE : GL_UNSIGNED_SHORT;
                 break;
@@ -182,7 +184,7 @@ rs2::frame decimation_filter_gl::process_frame(const rs2::frame_source& src, con
             {
                 glGenTextures(1, &work_texture);
                 glBindTexture(GL_TEXTURE_2D, work_texture);
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, _width, _height, 0, GL_RG, GL_UNSIGNED_BYTE, f.get_data());
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, _padded_width, _padded_height, 0, GL_RG, GL_UNSIGNED_BYTE, f.get_data());
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             }
@@ -190,11 +192,11 @@ rs2::frame decimation_filter_gl::process_frame(const rs2::frame_source& src, con
             uint32_t output_decimated;
             gf->get_gpu_section().output_texture(0, &output_decimated, TEXTYPE_RGB);
             glBindTexture(GL_TEXTURE_2D, output_decimated);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _padded_width, _padded_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-            gf->get_gpu_section().set_size(_width, _height);
+            gf->get_gpu_section().set_size(_padded_width, _padded_height);
 
             glBindFramebuffer(GL_FRAMEBUFFER, _fbo->get());
             glDrawBuffer(GL_COLOR_ATTACHMENT0);
@@ -208,7 +210,7 @@ rs2::frame decimation_filter_gl::process_frame(const rs2::frame_source& src, con
 
             auto& shader = (decimation_shader&)_viz->get_shader();
             shader.begin();
-            shader.set_size(_width, _height);
+            shader.set_size(_padded_width, _padded_height);
             shader.end();
 
             _viz->draw_texture(work_texture);
--- src/gl/decimation-gl.h
+++ src/gl/decimation-gl.h
@@ -1,20 +1,15 @@
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 
 #pragma once
 
-#include <map>
-#include <vector>
+//#include <memory>
+//#include <map>
+//#include <vector>
 
 #include "proc/decimation-filter.h"
-//#include "proc/synthetic-stream.h"
 #include "synthetic-stream-gl.h"
-
-
-#include <librealsense2/rs.hpp>
-#include "opengl3.h"
-
-#include <memory>
+//#include <librealsense2/rs.hpp>
 
 namespace rs2
 {
@@ -38,10 +33,6 @@ namespace librealsense
             rs2::frame process_frame(const rs2::frame_source& source, const rs2::frame& f) override;
         private:
             int _enabled = 0;
-            rs2::stream_profile _input_profile;
-            rs2::stream_profile _output_profile;
-
-            int _width, _height;
 
             std::shared_ptr<rs2::visualizer_2d> _viz;
             std::shared_ptr<rs2::fbo> _fbo;
