613fb4ad36329cd26d5c51e048a427546ebb8db4
Evgeni Raikhel <evgeni.raikhel@intel.com>
2020-07-23 17:17:43 +0300

    Fix multi-pin streaming by separating the callback context
    Change wait_for_frames() default timeout to 5 sec

--- src/win7/win7-uvc.cpp
+++ src/win7/win7-uvc.cpp
@@ -574,11 +574,9 @@ namespace librealsense
         // receive the original callback and pass it to the right device. this is registered by libUVC.
         static void internal_winusb_uvc_callback(frame_object *frame, void *ptr)
         {
-            std::cout << 1 << std::endl;
             callback_context *context = (callback_context *)ptr;
             //win7_uvc_device *device = context->_this;
             context->_callback(context->_profile, *frame, []() mutable {});
-            std::cout << 2 << std::endl;
         }
 
         void win7_uvc_device::play_profile(stream_profile profile, frame_callback callback)
@@ -632,12 +630,18 @@ namespace librealsense
 
             winusb_free_formats(formats);
 
-            _cb_context = std::make_shared<callback_context>();
+            callback_context *context = new callback_context();
+            context->_callback = callback;
+            context->_this = this;
+            context->_profile = profile;
+            auto sts = winusb_start_streaming(_device.get(), &ctrl, internal_winusb_uvc_callback, context, 0);
+
+            /*_cb_context = std::make_shared<callback_context>();
             _cb_context->_callback = callback;
             _cb_context->_profile = profile;
             _cb_context->_this = this;
 
-            auto sts = winusb_start_streaming(_device.get(), &ctrl, internal_winusb_uvc_callback, _cb_context.get(), 0);
+            auto sts = winusb_start_streaming(_device.get(), &ctrl, internal_winusb_uvc_callback, _cb_context.get(), 0);*/
             if(sts != UVC_SUCCESS)
                 throw std::runtime_error("Failed to start streaming!");
         }
